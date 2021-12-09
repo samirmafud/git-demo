091a8373de7353f68e29404255b3383129a7005e
Ruiz <samiruiz@deloittemx.com>
2021-12-09 14:26:05 -0600

    Feature: Agreando característica de suma: SR

--- src/app.py
+++ src/app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hola mundo"
+
+@app.route('/sum/<int:a>/<int:b>')
+def sum(a: int, b: int):
+    nums_sum = a + b
+    return f"La suma de los dos números es: + {str(nums_sum)}"
